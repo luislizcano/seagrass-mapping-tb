10ce9e6a19455e847d2d81b2f4574ddcbcabf4b6
Luis Lizcano-Sandoval <49037033+luislizcano@users.noreply.github.com>
2024-12-26 12:08:02 -0500

    Update functions.py

--- Preprocessing/functions.py
+++ Preprocessing/functions.py
@@ -7,16 +7,18 @@ def rescale(image):
   return img.copyProperties(image, image.propertyNames())
 
 ## Function to mask land (with feature collection) in an image collection
-def maskLand(geometry):
+def maskLand(imgColl, geometry):
   def apply(image):
     mask = ee.Image.constant(1).clip(geometry).mask().Not()
     return image.updateMask(mask)
+  return imgColl.map(apply)
 
 ## Function to clip an image collection by REGION
-def clipRegion(geometry):
+def clipRegion(imgColl, geometry):
   def apply(image):
     maskRegion = ee.Image.constant(1).clip(geometry).mask()
     return image.updateMask(maskRegion)
+  return imgColl.map(apply)
 
 ## Function to clean joins.
 def cleanJoin(feature):
@@ -29,57 +31,60 @@ def NDTI(image):
 
 ## Statistics function. It retrieves the mean and std values of the bands of interest 
 ## from each image in the collection.
-def getStats(img,region):
-  ## Select and rename bands.
-  image = ee.Image(img)
-  ## Create and combine reducers of mean and std.
-  reducers = ee.Reducer.mean().combine(**{
-    'reducer2': ee.Reducer.stdDev(),
-    'sharedInputs': True})
-  ## Apply reducers to the image.
-  stats = image.reduceRegion(**{
-    'reducer': reducers,
-    'geometry': region,
-    'scale': 10,
-    'maxPixels': 1e13})
-  ## Create feature variable to allocate data.
-  feature = ee.Feature(None)
-  ## Set properties of interest.
-  properties = ['system:index','MGRS_TILE','GENERATION_TIME','MEAN_SOLAR_ZENITH_ANGLE',\
-                'MEAN_SOLAR_AZIMUTH_ANGLE','SPACECRAFT_NAME','CLOUDY_PIXEL_PERCENTAGE',\
-                '704_mean','704_stdDev','NDTI_mean','NDTI_stdDev']
-  ## Apply the stats on every image and collect the output.
-  imageStats = image.setMulti(stats)
-  ## return feature with the selected properties.
-  return ee.Feature(feature).copyProperties(imageStats, properties)
+def getStats(imgColl, region):
+    ## Select and rename bands.
+    image = ee.Image(img)
+    ## Create and combine reducers of mean and std.
+    reducers = ee.Reducer.mean().combine(**{
+      'reducer2': ee.Reducer.stdDev(),
+      'sharedInputs': True})
+    ## Apply reducers to the image.
+    stats = image.reduceRegion(**{
+      'reducer': reducers,
+      'geometry': region,
+      'scale': 10,
+      'maxPixels': 1e13})
+    ## Create feature variable to allocate data.
+    feature = ee.Feature(None)
+    ## Set properties of interest.
+    properties = ['system:index','MGRS_TILE','GENERATION_TIME','MEAN_SOLAR_ZENITH_ANGLE',\
+                  'MEAN_SOLAR_AZIMUTH_ANGLE','SPACECRAFT_NAME','CLOUDY_PIXEL_PERCENTAGE',\
+                  '704_mean','704_stdDev','NDTI_mean','NDTI_stdDev']
+    ## Apply the stats on every image and collect the output.
+    imageStats = image.setMulti(stats)
+    ## return feature with the selected properties.
+    return ee.Feature(feature).copyProperties(imageStats, properties)
+  return imgColl.map(apply)
 
 ## Function for counting pixels
-def countPixels(img,region):
-  count = ee.Image(img).select(['704']).rename('Valid_Pixels').reduceRegion(**{
-    'reducer': ee.Reducer.count(), 
-    'scale': 10,
-    'geometry': region,
-    'maxPixels': 1e13})
-  ##Create empty feature collection to populate it with properties.  
-  feature = ee.Feature(None)
-  imageStats = img.setMulti(count)
-  properties = ['system:index','Valid_Pixels']
-  ##Collection with values
-  validPixels = ee.Feature(feature).copyProperties(imageStats,properties)
-  ##Extract the number of valid pixels
-  getValid = ee.Number(validPixels.get('Valid_Pixels'))
-  ##Create a feature with zero's mean and std values
-  zerosDef = {'704_mean': ee.Number(0), '704_stdDev': ee.Number(0),\
-                'NDTI_mean': ee.Number(0), 'NDTI_stdDev': ee.Number(0)}
-  zeroValues = ee.Feature(None, zerosDef)
-  ##Function to identify images with zero pixels and add the missing mean and std values (zero).
-  newCount = ee.Algorithms.If(**{
-     'condition': getValid.neq(0), 
-     'trueCase': validPixels, 
-     'falseCase': validPixels.copyProperties(zeroValues,['704_mean','704_stdDev',\
-      'NDTI_mean','NDTI_stdDev'])})
-  return newCount
-
+def countPixels(imgColl, region):
+  def apply(img):
+    count = ee.Image(img).select(['704']).rename('Valid_Pixels').reduceRegion(**{
+      'reducer': ee.Reducer.count(), 
+      'scale': 10,
+      'geometry': region,
+      'maxPixels': 1e13})
+    ##Create empty feature collection to populate it with properties.  
+    feature = ee.Feature(None)
+    imageStats = img.setMulti(count)
+    properties = ['system:index','Valid_Pixels']
+    ##Collection with values
+    validPixels = ee.Feature(feature).copyProperties(imageStats,properties)
+    ##Extract the number of valid pixels
+    getValid = ee.Number(validPixels.get('Valid_Pixels'))
+    ##Create a feature with zero's mean and std values
+    zerosDef = {'704_mean': ee.Number(0), '704_stdDev': ee.Number(0),\
+                  'NDTI_mean': ee.Number(0), 'NDTI_stdDev': ee.Number(0)}
+    zeroValues = ee.Feature(None, zerosDef)
+    ##Function to identify images with zero pixels and add the missing mean and std values (zero).
+    newCount = ee.Algorithms.If(**{
+       'condition': getValid.neq(0), 
+       'trueCase': validPixels, 
+       'falseCase': validPixels.copyProperties(zeroValues,['704_mean','704_stdDev',\
+        'NDTI_mean','NDTI_stdDev'])})
+    return newCount
+  return imgColl.map(apply)
+  
 ## Load Cloud Mask function for Sentinel-2 Images
 # =============================================================================
 # Function to mask clouds using band thresholds.
@@ -103,13 +108,13 @@ def countPixels(img,region):
 ## A helper to apply an expression and linearly rescale the output.
 ## Used in the landsatCloudScore function.
 def threshold(img, exp, thresholds):
-    return (img.expression(exp, {'img': img})
-      .subtract(thresholds[0]).divide(thresholds[1] - thresholds[0]))
+  return (img.expression(exp, {'img': img})
+    .subtract(thresholds[0]).divide(thresholds[1] - thresholds[0]))
  
 ## For BOA Sentinel-2 images, processed with the Py6S model 
 ## Adapted according to Chastain et al. 2019 (https://doi.org/10.1016/j.rse.2018.11.012).
 ## Compute a cloud score:
-def CloudScore6S(cloudThresh):
+def CloudScore6S(imgColl, cloudThresh):
   def apply(img):
     cloudThresh = int(cloudThresh)
     ## Compute several indicators of cloudyness and take the minimum of them.
@@ -140,3 +145,4 @@ def CloudScore6S(cloudThresh):
     score = score.lt(cloudThresh).rename('cloudMask')
     img = img.updateMask(img.mask().And(score))
     return ee.Image(img).addBands(score)
+  return imgColl.map(apply)
